43f722ba19103fe3cdf508264b80228aa3059948
Oscar Sanz <osanzce@gmail.com>
2021-12-01 12:22:25 +0100

    FEAT: renders post modal form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
+import { useContext } from 'react';
+import { useSelector } from 'react-redux';
 import Feed from '../components/Feed/Feed';
 import Header from '../components/Header/Header';
 import LeftSidebar from '../components/LeftSidebar/LeftSidebar';
 import RightSidebar from '../components/RightSidebar/RightSidebar';
+import PostModalContext from '../context/PostModalContext';
+import PostModal from '../components/PostModal/PostModal';
 import './Home.css';
 const Home = (props) => {
+  const { user } = useSelector((state) => state.auth.user);
+  const { isOpen, setIsOpen, isEditing, setIsEditing, postId, setPostId } = useContext(PostModalContext);
+
+  // console.log('postId', postId);
+
   return (
     <>
       <Header />
@@ -12,6 +21,7 @@ const Home = (props) => {
         <Feed className='feed' />
         <RightSidebar className='right__sidebar' />
       </div>
+      {isOpen && <PostModal className='post__modal' isEditing={isEditing ? isEditing : null} postId={postId} />}
     </>
   );
 };
