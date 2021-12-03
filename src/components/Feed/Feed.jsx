5ded03342e5cd17211e470a8fa41589b9904c3ee
Oscar Sanz <osanzce@gmail.com>
2021-12-03 21:12:53 +0100

    REFACTOR: force re-render component

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -10,16 +10,15 @@ import { getTimeLineAsync } from '../../redux/slices/post.slice';
 import { useState } from 'react';
 const Feed = () => {
   const { user } = useSelector((state) => state.auth.user);
-  const { posts } = useSelector((state) => state.post);
+  const { posts, postDetails } = useSelector((state) => state.post);
 
   const [isOpen, setIsOpen] = useState(false);
-  // console.log(isOpen);
 
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getTimeLineAsync(user._id));
-  }, [dispatch]);
+  }, [dispatch, postDetails]);
 
   return (
     <>
@@ -27,7 +26,7 @@ const Feed = () => {
         <div className='feed__container'>
           <Share setIsOpen={setIsOpen} />
 
-          {posts &&
+          {posts.length > 0 &&
             posts?.map((post) => {
               return <Post post={post} key={post._id} />;
             })}
