b2fb658380353f184685d731fd8fb82b4d4d584d
Oscar Sanz <osanzce@gmail.com>
2022-01-18 18:37:16 +0100

    FIX: adds https to socket connection method

--- src/components/Socket/Socket.jsx
+++ src/components/Socket/Socket.jsx
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('ws://node-social-face.herokuapp.com/', {
+export const socket = io('https://node-social-face.herokuapp.com/', {
   withCredentials: true,
   forceNew: true,
 });
