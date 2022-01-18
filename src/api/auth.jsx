f3352eb23768dd4998e947c1ae812f92fd24ef4b
Oscar Sanz <osanzce@gmail.com>
2022-01-18 18:25:46 +0100

    FIX: adds https to checkSession method

--- src/api/auth.jsx
+++ src/api/auth.jsx
@@ -59,7 +59,7 @@ export const checkSession = async () => {
     header: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '//social-face.netlify.app/',
+      'Access-Control-Allow-Origin': 'https://social-face.netlify.app/',
     },
     credentials: 'include',
   });
