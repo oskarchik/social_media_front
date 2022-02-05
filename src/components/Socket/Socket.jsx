711e7e0b1c6c9fcb47bca1727fa13ac1800f66f7
Oscar Sanz <osanzce@gmail.com>
2022-02-05 18:39:23 +0100

    CONFLICT: solve merge conflict in Socket

--- src/components/Socket/Socket.jsx
+++ src/components/Socket/Socket.jsx
@@ -1,8 +1,5 @@
 import { io } from 'socket.io-client';
 
-<<<<<<< HEAD
-export const socket = io('ws:https://node-social-face.herokuapp.com/', {
-=======
 export const socketUrl =
   process.env.NODE_ENV === 'development'
     ? `${process.env.REACT_APP_SOCKET_URL_DEV}`
@@ -10,7 +7,6 @@ export const socketUrl =
     ${process.env.REACT_APP_API_URL_PROD}`;
 
 export const socket = io(socketUrl, {
->>>>>>> fix-refactor
   withCredentials: true,
   forceNew: true,
   extraHeaders: {
