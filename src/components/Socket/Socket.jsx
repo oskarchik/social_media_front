f477b6e659f67f56ae373366878d680cf1b741c7
Oscar Sanz <osanzce@gmail.com>
2022-02-05 19:01:36 +0100

    FIX: testing socket url

--- src/components/Socket/Socket.jsx
+++ src/components/Socket/Socket.jsx
@@ -4,7 +4,7 @@ export const socketUrl =
   process.env.NODE_ENV === 'development'
     ? `${process.env.REACT_APP_SOCKET_URL_DEV}`
     : `
-    ${process.env.REACT_APP_API_URL}`;
+    ${process.env.REACT_APP_SOCKET_URL}`;
 
 export const socket = io(socketUrl, {
   withCredentials: true,
