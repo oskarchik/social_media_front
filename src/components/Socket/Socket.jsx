e2da762c9961ee5ed0314c5ca8bfdedb991ee880
Oscar Sanz <osanzce@gmail.com>
2022-02-06 20:00:45 +0100

    socket url from netlify env

--- src/components/Socket/Socket.jsx
+++ src/components/Socket/Socket.jsx
@@ -8,7 +8,7 @@ export const socketUrl =
 console.log(process.env.REACT_APP_SOCKET_URL);
 console.log(socketUrl);
 
-export const socket = io('https://social-face.netlify.app', {
+export const socket = io(socketUrl, {
   withCredentials: true,
   forceNew: true,
   extraHeaders: {
