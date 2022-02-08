49fe8e822a4dcd51513794cb345d0362e03b9700
Oscar Sanz <osanzce@gmail.com>
2022-02-08 22:38:52 +0100

    REFACTOR: renames isOpen variable for isOpenPostModal

--- src/context/PostModalContext.jsx
+++ src/context/PostModalContext.jsx
@@ -3,11 +3,11 @@ import { createContext, useState } from 'react';
 const PostModalContext = createContext();
 
 export const PostModalProvider = ({ children }) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpenPostModal, setIsOpenPostModal] = useState(false);
   const [mode, setMode] = useState('');
   const [postId, setPostId] = useState('');
   return (
-    <PostModalContext.Provider value={{ isOpen, setIsOpen, mode, setMode, postId, setPostId }}>
+    <PostModalContext.Provider value={{ isOpenPostModal, setIsOpenPostModal, mode, setMode, postId, setPostId }}>
       {children}
     </PostModalContext.Provider>
   );
